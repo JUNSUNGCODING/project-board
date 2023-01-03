8f14e5e5b88d9e70ea092062edf21762e52f59f5
JUNSUNG <hjs5038@gmail.com>
2023-01-03 23:32:12 +0900

    #10 수정
    수정

--- src/main/java/com/firstproject/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/firstproject/projectboard/repository/ArticleRepository.java
@@ -4,6 +4,7 @@ import com.firstproject.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
 @RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
