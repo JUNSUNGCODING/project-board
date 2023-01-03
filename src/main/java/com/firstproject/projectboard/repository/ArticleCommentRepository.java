daa47d2e3bb95ad9e780122f71cf6b3717210ff6
JUNSUNG <hjs5038@gmail.com>
2023-01-03 23:30:53 +0900

    #10 - gitignore 설정2
    바이너리파일 생성되서 gitignore 수정

--- src/main/java/com/firstproject/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/firstproject/projectboard/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.firstproject.projectboard.repository;
 
 import com.firstproject.projectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
--- src/main/java/com/firstproject/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/firstproject/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.firstproject.projectboard.repository;
 
 import com.firstproject.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
